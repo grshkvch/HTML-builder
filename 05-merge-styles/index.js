383d40f91b6f638d986e426717e3415fc79d09fa
grshkvch <volody_200115@mail.ru>
2022-05-24 02:26:07 +0300

    feat: done 5 task (Tue, May 24, 2022 02:26:07 AM)

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const stylesFolder = path.join(__dirname, 'styles');
+const bundleCSS = path.join(__dirname, 'project-dist', 'bundle.css');
+
+async function toBundle () {
+    await fs.readdir(stylesFolder, {withFileTypes: true}, (err, data) => {
+        if (err) throw err
+        fs.promises.writeFile(bundleCSS, '')
+        for (const style of data) {
+            if (style.name.endsWith('.css') && style.isFile()) {
+                const readableStream = fs.createReadStream(path.join(stylesFolder, style.name))
+                readableStream.on('data', chunk => {
+                    fs.promises.appendFile(bundleCSS, chunk)
+                })
+            }
+        }
+    })
+}
+
+toBundle()
